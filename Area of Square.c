ac93548ff9293716d0771c5a6b8608af654dc867
galioTM <100420536+galioTM@users.noreply.github.com>
2022-03-20 11:31:45 +0000

    Add files via upload

---
+++ Area of Square.c
@@ -0,0 +1,17 @@
+
+	/* Used to Find the area of Squre */
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int s;
+float a;
+printf("Enter s value:- ");
+scanf("%d",&s);
+printf("Total Area = ");
+a=s*s;
+printf("%f",a);
+return 0;
+}
+//Credits:- Pavan , Venkatesh
+}

602bb91286a5e96ef292c07d2034c4221f2b4f76
galioTM <100420536+galioTM@users.noreply.github.com>
2022-03-20 11:35:53 +0000

    Add files via upload

---
+++ Calculator (+or-).c
@@ -0,0 +1,17 @@
+//Author:Venkatesh
+#include<stdio.h>
+int main(){
+	/*Simple Addition and Subtraction Print Calculator 
+	made by me */
+	int a,b,c;
+printf("Enter two numbers\n");
+printf("First number:-");
+scanf("%d",&a);
+printf("Secound number:-");
+scanf("%d",&b);
+ c=a+b;
+ 
+ printf(" Sum of two numbers =%d \n",c);
+ printf("Created by Venkatesh");
+}
+ 

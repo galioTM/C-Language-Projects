b685ad88a3db051f8116a3887db64bcb451e0b93
galioTM <100420536+galioTM@users.noreply.github.com>
2022-03-20 11:33:57 +0000

    Add files via upload

---
+++ Circumstances of Circle.c
@@ -0,0 +1,27 @@
+/* Used to Find Circumfernce of Cicle
+Formula:- C= 2*Pie*r */
+#include <stdio.h>
+#include <math.h>
+#define pip 3.14
+ int main () {
+ 	int r;
+ 	float c;
+ 	
+ 	 
+ 	printf("Enter Values");
+ 	printf("Radius = ");
+ 	scanf("%d",&r);
+ 	printf("Circumfernce of Circle = ");
+ 	c =2*pip*r;
+ 	printf("%f",&c);
+ 	return 0;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
